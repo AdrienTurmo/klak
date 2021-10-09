fafd8acf59126fb9cea30c9c327aa7f9eea37ba3
AdrienTurmo <adrien.turmo@gmail.com>
2021-10-10 00:47:38 +0200

    Un peu de style sur les modal

--- src/components/organisms/ArmyCreation/ArmyCreation.tsx
+++ src/components/organisms/ArmyCreation/ArmyCreation.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './ArmyCreation.module.scss';
-import { Button, Modal, UnitLine, useArmyCreationContext } from 'components';
+import { Button, Modal, Separator, UnitLine, useArmyCreationContext } from 'components';
 
 interface UnitCategory {
   type: UnitType;
@@ -8,34 +8,35 @@ interface UnitCategory {
   modalTitle: string;
 }
 
+const unitCategories: UnitCategory[] = [
+  {
+    type: 'BASE',
+    title: 'Unitées de bases',
+    modalTitle: 'Ajouter une unité de base',
+  },
+  {
+    type: 'SPECIAL',
+    title: 'Unitées spéciales',
+    modalTitle: 'Ajouter une unité spéciale',
+  },
+  {
+    type: 'RARE',
+    title: 'Unitées rares',
+    modalTitle: 'Ajouter une unité rare',
+  },
+  {
+    type: 'HERO',
+    title: 'Héros',
+    modalTitle: 'Ajouter un héro',
+  },
+  {
+    type: 'LORD',
+    title: 'Seigneurs',
+    modalTitle: 'Ajouter un seigneur',
+  },
+];
+
 export const ArmyCreation: React.FC = () => {
-  const unitCategories: UnitCategory[] = [
-    {
-      type: 'BASE',
-      title: 'Unitées de bases',
-      modalTitle: 'Ajouter une unité de base',
-    },
-    {
-      type: 'SPECIAL',
-      title: 'Unitées spéciales',
-      modalTitle: 'Ajouter une unité spéciale',
-    },
-    {
-      type: 'RARE',
-      title: 'Unitées rares',
-      modalTitle: 'Ajouter une unité rare',
-    },
-    {
-      type: 'HERO',
-      title: 'Héros',
-      modalTitle: 'Ajouter un héro',
-    },
-    {
-      type: 'LORD',
-      title: 'Seigneurs',
-      modalTitle: 'Ajouter un seigneur',
-    },
-  ];
   const [categoryOfUnitToAdd, setCategoryOfUnitToAdd] = useState<UnitCategory>();
   const { addUnit, getUnits, getArmyUnits, calculateTotalArmyPoints } = useArmyCreationContext();
 
@@ -48,28 +49,35 @@ export const ArmyCreation: React.FC = () => {
     <>
       <div className={styles.ArmyCreation} data-testid="ArmyCreation">
         {unitCategories.map((unitCategory) => (
-          <>
-            <div>{unitCategory.title}</div>
-            <Button onClick={() => setCategoryOfUnitToAdd(unitCategory)}>+</Button>
-            {getArmyUnits(unitCategory.type).map((armyUnit, index) => (
-              <UnitLine armyUnit={armyUnit} key={index} />
-            ))}
-          </>
+          <div key={unitCategory.type} className={styles.ArmyCategory}>
+            <div className={styles.ArmyCategoryHeader}>
+              <div className={styles.ArmyCategoryTitle}>{unitCategory.title}</div>
+              <Button className={styles.ArmyCategoryAddButton} onClick={() => setCategoryOfUnitToAdd(unitCategory)}>
+                +
+              </Button>
+            </div>
+            <div className={styles.ArmyCategoryUnits}>
+              {getArmyUnits(unitCategory.type).map((armyUnit, index) => (
+                <UnitLine armyUnit={armyUnit} key={index} />
+              ))}
+            </div>
+            <Separator />
+          </div>
         ))}
-      </div>
 
-      <div>Total: {calculateTotalArmyPoints() | 0}</div>
+        <div>Total: {calculateTotalArmyPoints() | 0}</div>
+      </div>
 
       {categoryOfUnitToAdd && (
         <Modal onClickClose={() => setCategoryOfUnitToAdd(undefined)} title={categoryOfUnitToAdd.modalTitle}>
-          <>
-            <div>Unité à ajouter</div>
+          <div className={styles.AddUnitModal}>
+            <div className={styles.AddUnitModalTitle}>Unité à ajouter</div>
             {getUnits(categoryOfUnitToAdd.type).map((unit) => (
-              <Button key={unit.name} onClick={onClickAddUnit(unit)}>
+              <Button className={styles.AddUnitModalButton} key={unit.name} onClick={onClickAddUnit(unit)}>
                 {unit.name}
               </Button>
             ))}
-          </>
+          </div>
         </Modal>
       )}
     </>
