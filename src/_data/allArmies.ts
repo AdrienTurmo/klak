fb80f038f6188dfde3196eb217e4aedfab7260a0
AdrienTurmo <adrien.turmo@gmail.com>
2022-05-14 00:05:46 +0200

    Modification affichage sélection armée/version

--- src/_data/allArmies.ts
+++ src/_data/allArmies.ts
@@ -1,9 +1,11 @@
 import { ComtesVampiresV6 } from '_data/V6/comtesVampiresV6';
+import { OrcsV6 } from '_data/V6/orcsV6';
+import { ElfsSylvainsV6 } from '_data/V6/ElfsSylvainsV6';
 
 export const Versions = ['V6', 'V7', 'V8'];
 
 export const AllArmies: Map<string, Army[]> = new Map([
-  ['V6', [ComtesVampiresV6]],
-  ['V7', []],
-  ['V8', []],
+  ['V6', [ComtesVampiresV6, OrcsV6, ElfsSylvainsV6]],
+  ['V7', [ComtesVampiresV6, OrcsV6, ElfsSylvainsV6]],
+  ['V8', [ComtesVampiresV6, OrcsV6, ElfsSylvainsV6]],
 ]);
--- src/components/organisms/CreateNewArmy/CreateNewArmy.tsx
+++ src/components/organisms/CreateNewArmy/CreateNewArmy.tsx
@@ -1,77 +1,37 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'components';
 import styles from './CreateNewArmy.module.scss';
+import { Button } from 'components';
 import { AllArmies, Versions } from '_data/allArmies';
-
-type ArmyCreationStates = 'SELECT_VERSION' | 'SELECT-ARMY';
 
 interface Props {
   onArmySelect: (version: string, army: Army) => void;
 }
 
 export const CreateNewArmy: React.FC<Props> = ({ onArmySelect }) => {
-  const [version, setVersion] = useState('');
   const [openCreateArmy, setOpenCreateArmy] = useState(false);
-  const [armyCreationState, setArmyCreationState] = useState<ArmyCreationStates>('SELECT_VERSION');
-  const [chosenVersionArmies, setChosenVersionArmies] = useState<Army[]>([]);
 
-  const selectVersion = (version: string) => () => {
-    const armiesOfVersion: Army[] | undefined = AllArmies.get(version);
-    if (!armiesOfVersion) return;
-    setVersion(version);
-    setChosenVersionArmies(armiesOfVersion);
-    setArmyCreationState('SELECT-ARMY');
-  };
-
-  const VersionChoice = (
-    <>
-      <div>Sélectionnez une version</div>
-      {Versions.map((version) => (
-        <Button onClick={selectVersion(version)} key={version}>
-          {version}
-        </Button>
-      ))}
-      <div />
-    </>
-  );
-
-  const selectArmy = (army: Army) => () => {
-    onArmySelect(version, army);
-    closeModal();
-  };
-
-  const ArmyChoice = (
-    <>
-      <div>Sélectionnez une armée</div>
-      {chosenVersionArmies.map((army) => (
-        <Button key={army.name} onClick={selectArmy(army)}>
-          {army.name}
-        </Button>
-      ))}
-      <div />
-    </>
-  );
-
-  const getStateDisplay = () => {
-    if (armyCreationState === 'SELECT_VERSION') {
-      return VersionChoice;
-    } else {
-      return ArmyChoice;
-    }
-  };
-
-  const closeModal = () => {
-    setArmyCreationState('SELECT_VERSION');
-    setChosenVersionArmies([]);
-    setOpenCreateArmy(false);
-  };
   return (
     <>
       <Button onClick={() => setOpenCreateArmy(true)}>Créer une liste d&apos;armée</Button>
+
       {openCreateArmy && (
-        <Modal onClickClose={closeModal} title="Créer une nouvelle armée">
-          <div className={styles.NewArmyModal}>{getStateDisplay()}</div>
-        </Modal>
+        <div className={styles.AllArmiesList}>
+          {Versions.map((version) => (
+            <div key={version} className={styles.ArmiesList}>
+              <div>{version}</div>
+              {AllArmies.get(version)?.map((army) => (
+                <Button
+                  key={army.name}
+                  onClick={() => {
+                    onArmySelect(version, army);
+                  }}
+                >
+                  {army.name}
+                </Button>
+              ))}
+            </div>
+          ))}
+        </div>
       )}
     </>
   );
