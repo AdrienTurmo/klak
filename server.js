563a85de38169c6272ddbdf76b6539bdf49b56ab
AdrienTurmo <adrien.turmo@gmail.com>
2022-06-13 00:46:26 +0200

    Testing ssr

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 /* eslint-disable */
 const express = require('express');
 
+const PORT = process.env.PORT || 3006;
 const app = express();
 
-const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: 'adrie',
-  host: 'localhost',
-  database: 'toto',
-  password: 'root',
-  port: 5432,
-});
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//   user: 'adrie',
+//   host: 'localhost',
+//   database: 'toto',
+//   password: 'root',
+//   port: 5432,
+// });
+//
+// const toto = () =>
+//   pool.query('SELECT * from bipou where id = 1').then((response) => {
+//     console.log(response.rows);
+//   });
 
-pool.query('SELECT * from bipou where id = 1').then((response) => {
-  console.log(response.rows);
-});
+app
+  .use(express.static('./build'))
+  .use((req, res) => res.sendFile('/build/index.html'))
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
